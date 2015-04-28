f7a14ef1a6e402cd18536b7c56110a19b46d6fa4
maxamud maxamed <maxamud2006@gmail.com>
2015-04-28 16:06:04 +0100

    Added CSS in the client pages.

---
+++ src/java/ss/services.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ss;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author maxamud
+ */
+@WebService(serviceName = "services")
+public class services {
+}
